39d508116e590975daf130ac3d215ea7a74af2a5
LucasMiller98 <lucas.miller1998@outlook.com>
2021-08-24 01:02:33 -0300

    Backend

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    host: '127.0.0.1', 
+    // username: 'root',
+    // password: 'senha',
+    // database: 'contatos',
+    // dialect: 'mariadb',
+
+
+    username: 'root',
+    password: 'BHU*nji9',
+    database: 'contatos',
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+};
